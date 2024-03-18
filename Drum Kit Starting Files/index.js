8a13d0d68a5403e4064cb68c8e070cadd37e9326
Robert Rice <robertrice12009@yahoo.com>
2024-03-18 14:57:17 -0500

    changes

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,12 +1,28 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for(var i = 0; i<numberOfDrumButtons; i++){
+for (var i = 0; i<numberOfDrumButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){        
+        
+        var buttonInnerHTML = this.innerHTML;
 
-document.querySelectorAll(".drum").addEventListener("click",handleclick);
+        console.log(this);
+
+        switch (buttonInnerHTML) {
+            case "w":
+                var tom1 = new Audio ("sounds/tom-1.mp3")
+                tom1.play();
+                break;
+            case "a":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+
+            default:
+                break;
+        }
+    });
 }
 
-function handleclick(){
-    alert("I got clicked!");
-
-    //What to do when click detected
-}
+document.addEventListener("keypress", function() {
+    alert("Key was Pressed!");
+});
